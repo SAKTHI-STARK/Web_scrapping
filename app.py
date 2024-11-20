ee795bbeac588ff3fe84ee7e83deb4b0570d7a4c
SAKTHI-STARK <sakthi492003@gmail.com>
2024-11-20 06:38:55 +0530

    transfer html tags itself

--- app.py
+++ app.py
@@ -8,9 +8,8 @@ def index():
         product_name = request.form['product_name']
         product_name = (product_name)
         tags_from_site=s.call_website(product_name)
-        data = [element.text.strip() for element in tags_from_site]
-        print(data)
-        return render_template('results.html',data=data,product_name=product_name )
+        data = [str(element) for element in tags_from_site]
+        return render_template('results.html',data=tags_from_site,product_name=product_name )
     return render_template('index.html')
 
 
